ae9a7b1d0ffa3d92a14ad3beeb9641935e21d60a
chaos430 <1192318530@qq.com>
2021-11-26 14:20:19 +0800

    添加侧边栏样式

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,47 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+
+
+    <el-container class="home-container">
+<!--      头部-->
+      <el-header>
+        <div>
+          <img src="../assets/user.jpg" alt="" style="width: 100px;height: 100px">
+          <span>电商后台管理系统</span>
+        </div>
+        <el-button type="info" @click="logout">退出</el-button></el-header>
+      <el-container>
+<!--        主体-->
+        <el-aside width="200px">
+<!--侧边栏菜单区-->
+          <el-menu
+              background-color="#333744"
+              text-color="#fff"
+              active-text-color="#ffd04b">
+            <!--              一级菜单-->
+            <el-submenu index="1">
+              <!--                一级菜单模板-->
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+<!--              二级菜单-->
+              <el-menu-item>
+<!--                二级菜单模板-->
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+              </el-menu-item>
+            </el-submenu>
+
+          </el-menu>
+
+
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+
 </template>
 
 <script>
@@ -17,5 +57,29 @@ export default{
 </script>
 
 <style lang="less" scoped>
-
+.home-container{
+  height: 100%;
+}
+.el-header{
+  background-color: #373D41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color:#fff;
+  font-size: 20px;
+  > div{
+    display: flex;
+    align-items: center;
+    span{
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside{
+  background-color: #333744;
+}
+.el-main{
+  background-color: #eaedf1;
+}
 </style>
