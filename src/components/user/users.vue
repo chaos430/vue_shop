c49cf89d4541ed99327782ed236d41d176413ae9
chaos430 <1192318530@qq.com>
2021-11-29 13:21:43 +0800

    创建用户列表组件

---
+++ src/components/user/users.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h3>
+用户列表组件
+  </h3>
+
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -3,32 +3,36 @@ import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from "../components/Home";
 import Welcome from '../components/Welcome'
+import Users from '../components/user/users'
+
 Vue.use(VueRouter)
 
 
-
 const router = new VueRouter({
-  routes : [
-    {
-      path:'/',redirect:'/login'
-    },
-    {
-      path: '/login',component: Login
-    },
-    {
-      path:'/home',component:Home,
-      redirect:'/welcome',
-      children:[
-        {path:'/welcome',component:Welcome}
-      ]
-    }
-  ]
+    routes: [
+        {
+            path: '/', redirect: '/login'
+        },
+        {
+            path: '/login', component: Login
+        },
+        {
+            path: '/home', component: Home,
+            redirect: '/welcome',
+            children: [
+                {path: '/welcome', component: Welcome},
+                {
+                    path: '/users', component: Users
+                }
+            ]
+        }
+    ]
 })
 //挂载路由导航守卫
-router.beforeEach((to,from,next)=>{
-  if(to.path === '/login') return next()
-  const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
-  next()
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') return next()
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
 })
 export default router
