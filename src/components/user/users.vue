125f64fdf1540cd28d5c3c05180c73866197cfcf
chaos430 <1192318530@qq.com>
2021-11-29 15:30:11 +0800

    添加操作按钮

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -1,68 +1,76 @@
 <template>
-<div>
-<!--  面包屑导航-->
-  <el-breadcrumb separator-class="el-icon-arrow-right">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-    <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-  </el-breadcrumb>
-<!--卡片视图区-->
-  <el-card>
-<!--   搜索与添加区域-->
+  <div>
+    <!--  面包屑导航-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!--卡片视图区-->
+    <el-card>
+      <!--   搜索与添加区域-->
 
-    <el-row :gutter="20">
-      <el-col :span="8">
-        <el-input placeholder="请输入内容">
-          <el-button slot="append" icon="el-icon-search"></el-button>
-        </el-input>
-      </el-col>
-      <el-col :span="4">
-        <el-button type="primary">添加用户</el-button>
-      </el-col>
-    </el-row>
-<!--    用户列表区-->
-    <el-table :data="userList" border stripe>
-      <el-table-column type="index"></el-table-column>
-      <el-table-column label="姓名" prop="username"></el-table-column>
-      <el-table-column label="邮箱" prop="email"></el-table-column>
-      <el-table-column label="电话" prop="mobile"></el-table-column>
-      <el-table-column label="角色" prop="role_name"></el-table-column>
-      <el-table-column label="状态">
-        <template slot-scope="scope">
-          <el-switch
-              v-model="scope.row.mg_state">
-          </el-switch>
-        </template>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <!--    用户列表区-->
+      <el-table :data="userList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-switch
+                v-model="scope.row.mg_state">
+            </el-switch>
+          </template>
 
-      </el-table-column>
-      <el-table-column label="操作"></el-table-column>
-    </el-table>
-  </el-card>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="warning" icon="el-icon-share" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
 
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-  data(){
-return{
-  //获取用列表的参数对象
-  queryInfo:{
-    query:'',
-    pagenum:1,
-    pagesize:2
+  data() {
+    return {
+      //获取用列表的参数对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      },
+      userList: [],
+      total: 0
+    }
   },
-  userList:[],
-  total:0
-}
-  },
-  created(){
+  created() {
     this.getUserList()
   },
-  methods:{
-    async getUserList(){
-    const{data:res} = await this.$http.get('users',{params:this.queryInfo})
-      if(res.meta.status !== 200){return this.$message.error('获取用户列表失败')}
+  methods: {
+    async getUserList() {
+      const {data: res} = await this.$http.get('users', {params: this.queryInfo})
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取用户列表失败')
+      }
       this.userList = res.data.users
       this.total = res.data.total
     }
