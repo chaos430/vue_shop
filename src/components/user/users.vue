97bec0b024dc86b1e43befc5791d3912f9b19305
chaos430 <1192318530@qq.com>
2021-11-29 14:38:50 +0800

    获取用户列表数据

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -26,6 +26,31 @@
 </template>
 
 <script>
+export default {
+  data(){
+return{
+  //获取用列表的参数对象
+  queryInfo:{
+    query:'',
+    pagenum:1,
+    pagesize:2
+  },
+  userList:[],
+  total:0
+}
+  },
+  created(){
+    this.getUserList()
+  },
+  methods:{
+    async getUserList(){
+    const{data:res} = await this.$http.get('users',{params:this.queryInfo})
+      if(res.meta.status !== 200){return this.$message.error('获取用户列表失败')}
+      this.userList = res.data.users
+      this.total = res.data.total
+    }
+  }
+}
 
 </script>
 
