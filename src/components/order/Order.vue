d860f29a48d9bf9da774a8e9b780e28a8ff956a3
chaos430 <1192318530@qq.com>
2021-12-06 14:13:41 +0800

    获取订单列表数据

---
+++ src/components/order/Order.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <!--  面包屑导航-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+<!--    卡片视图-->
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容"  >
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+
+
+
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      queryInfo:{
+        query:'',
+        pagenum:1,
+        pagesize:10
+      },
+      total:0,
+      orderList:[]
+    }
+  },
+  created() {
+    this.getOrderList()
+  },
+  methods: {
+   async getOrderList(){
+   const{data:res} =  await this.$http.get('orders',{
+        params:this.queryInfo
+      })
+     if(res.meta.status!== 200){
+       return this.$message.error('获取订单列表失败')
+     }
+     this.total = res.data.total
+     this.orderList = res.data.goods
+    }
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -10,6 +10,7 @@ import Cate from '../components/goods/Cate'
 import Params from "../components/goods/Params";
 import GoodsList from '../components/goods/List'
 import Add from '../components/goods/Add'
+import Order from '../components/order/Order'
 Vue.use(VueRouter)
 
 
@@ -46,6 +47,9 @@ const router = new VueRouter({
                 },
                 {
                     path:'/goods/add',component:Add
+                },
+                {
+                    path:'/orders',component:Order
                 }
             ]
         }
